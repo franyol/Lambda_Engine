dfe12135bb45e4f4d6fab19e056a4c8a1b7036ad
franyol <f.valbuenao64@gmail.com>
2024-04-09 00:39:56 -0500

    fix typo

---
+++ tests/blend.cpp
@@ -0,0 +1,34 @@
+#include <lambda.h>
+
+int main ( int argc, char* argv[] ) {
+    LE_Init();
+    // Create a window to show the texture
+    Uint32 mainWindow = LE_TEXTURE->createWindow( "Blend", 640, 480 );
+
+    // Load two images into the texture manager
+    LE_TEXTURE->loadTexture( mainWindow, "myimage1.png", "im1" );
+    LE_TEXTURE->loadTexture( mainWindow, "myimage2.png", "im2" );
+    // Create tiles for those textures
+    LE_TEXTURE->createTile( mainWindow, "im1", "im1_tile" );
+    LE_TEXTURE->createTile( mainWindow, "im2", "im2_tile" );
+
+    // Create a new texture and tile to blend the tiles:
+    LE_TEXTURE->createTargetTexture( mainWindow, "im3", 640, 480 );
+    LE_TEXTURE->createTile( mainWindow, "im3", "im3_tile" );
+
+    // Now when we draw, we draw into the texture instead of the window
+    LE_TEXTURE->draw( mainWindow, "im1+tile", 0, 0, 640, 480, false );
+    LE_TEXTURE->draw( mainWindow, "im2_tile", 0, 0, 320, 240, false );
+
+    // Restore the target to the main window
+    LE_TEXTURE->restoreRenderTarget( mainWindow );
+
+    // display the blended texture into the window
+    LE_TEXTURE->draw( mainWindow, "im3_tile", 0, 0 );
+    LE_TEXTURE->present( mainWindow );
+
+    // Mantain the window during 5 seconds
+    SDL_Delay ( 5000 );
+    LE_Quit();
+    return 0;
+}
